b914533e8816bd0c67c8ff75d129f91ae4ac46c1
Trevor <trevor.h.crow@gmail.com>
2020-02-28 21:22:12 -0500

    added max iteration function with timer to it

--- src/main.rs
+++ src/main.rs
@@ -63,9 +63,33 @@ fn hill_climb_rand_restart(b: &board::Board) -> board::Board{
     init
 }
 
+fn hill_climb_rand_restart_max(b: &board::Board, max_iterations: u8) -> board::Board{
+    let mut init = b.clone();
+    let mut iteration = 0u8;
+    while init.score() != 0 && iteration != max_iterations{
+        let mut solution = init.clone();
+        for potential_solution in init.get_neighbors(){
+            if potential_solution.score() < solution.score(){
+                solution = potential_solution;
+            }
+        }
+        if init.is_peak_plateau(&solution){
+            init = board::Board::new(init.get_n(), create_board(init.get_n()));
+        }else{
+            init = solution;
+        }
+        iteration += 1;
+    }
+
+    init
+}
+
 fn main() {
     let n = 8;
     let mut size = 1;
+    let max_iterations = 50;
+    let mut timer = SystemTime::now();
+    let mut runtime = timer.elapsed().unwrap();
 
     for _i in 0..5{
         size *= 10;
@@ -78,13 +102,15 @@ fn main() {
         let mut hc_success_rate = 0.0;
         let mut hcrr_avg_score = 0.0;
         let mut hcrr_success_rate = 0.0;
+        let mut hcrr_max_avg_score = 0.0;
+        let mut hcrr_max_success_rate = 0.0;
 
         /*
         TODO:
         add in timer to track how many nanoseconds pass for hill climb and hill climb rand restart for doing all of the tables
         */ 
 
-        let mut timer = SystemTime::now();
+        timer = SystemTime::now();
 
         // hill climb
         for table in &tables{
@@ -96,7 +122,7 @@ fn main() {
             }
         }
 
-       let mut runtime = timer.elapsed().unwrap();
+       runtime = timer.elapsed().unwrap();
 
         hc_avg_score = hc_avg_score/(size as f64);
         hc_success_rate = (hc_success_rate/(size as f64)) * 100.0;
@@ -118,6 +144,23 @@ fn main() {
         hcrr_avg_score = hcrr_avg_score/(size as f64);
         hcrr_success_rate = (hcrr_success_rate/(size as f64)) * 100.0;
         println!("After Hill Climb Rand Restart (Avg Score): {}\nAfter Hill Climb Rand Restart (Success Rate): {}%\nHill Climb Rand Restart Runtime: {:?}\n", hcrr_avg_score, hcrr_success_rate, runtime);
+        
+        timer = SystemTime::now();
+        // hill climb rand restart with max iteration limit
+        for table in &tables{
+            let hcrr_max_table = hill_climb_rand_restart_max(&table, max_iterations);
+    
+            hcrr_max_avg_score += hcrr_max_table.score() as f64;
+            if hcrr_max_table.score() == 0{
+                hcrr_max_success_rate += 1.0;
+            }
+        }
+        runtime = timer.elapsed().unwrap();
+
+        hcrr_max_avg_score = hcrr_max_avg_score/(size as f64);
+        hcrr_max_success_rate = (hcrr_max_success_rate/(size as f64)) * 100.0;
+        println!("After Hill Climb Rand Restart w/ max_iterations = {} (Avg Score): {}\nAfter Hill Climb Rand Restart w/ max_iterations = {} (Success Rate): {}%\nHill Climb Rand Restart w/ max_iterations = {} Runtime: {:?}\n", max_iterations, hcrr_max_avg_score, max_iterations, hcrr_max_success_rate, max_iterations, runtime);
+
         println!("\n---------------------------------------------------------------------------------\n");
     }
 }
